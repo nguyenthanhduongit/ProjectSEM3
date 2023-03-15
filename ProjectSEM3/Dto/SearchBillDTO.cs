90e087ebc9162994bf368bc42a92e1d953e6e05e
nguyenthanhduongit <nguyenduong29005@gmail.com>
2023-03-15 20:26:56 +0700

    update

---
+++ ProjectSEM3/Dto/SearchBillDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSEM3.Dto
+{
+    public class SearchBillDTO
+    {
+        public string ProductName { get; set; }
+        public string CustomerName { get; set; }
+        public int Phone { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ ProjectSEM3/Dto/SearchCustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSEM3.Dto
+{
+    public class SearchCustomerDTO
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public int Phone { get; set; }
+    }
+}
